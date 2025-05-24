0a0e04383271b137b2fbe7636374bb27ec8da7af
Daniel-Otieno <oderodanielotieno@gmail.com>
2025-05-24 16:14:08 +0300

    New commit

---
+++ for loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Print numbers 1 to 5
+    for (int i = 1; i <= 5; i++) {
+        printf("Number: %d\n", i);  // Print current value of i
+    }
+
+    return 0;
+}
---
+++ while Loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    // Keep printing while i is less than or equal to 5
+    while (i <= 5) {
+        printf("Count: %d\n", i);
+        i++;  // Increase i by 1
+    }
+
+    return 0;
+}
